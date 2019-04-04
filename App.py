31641cf8b768914c09322d459e362f2754a81aa2
skr47ch <its.me.arrived@gmail.com>
2019-04-04 23:43:03 +1100

    Created graph on activity data using Matplotlib

--- App.py
+++ App.py
@@ -1,20 +1,53 @@
 import MemriseLogin
+import mycredentials
 
-new_login = MemriseLogin.Login('test_skr47ch', 'test_password')
-course_list = new_login.get_course()
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
-for course in course_list['courses']:
-    id_ = course['id']
-    name_ = course['name']
+username = mycredentials.username
+password = mycredentials.password
+new_login = MemriseLogin.Login(username, password)
 
-    # some courses may not have goals attached to them.
-    try:
-        goal_ = course['goal']['goal']
-        points_ = course['goal']['points']
-        streak_ = course['goal']['streak']
-    except KeyError:
-        goal_, points_, streak_ = None, None, None
 
-    progress_ = str(points_) + "/" + str(goal_)
+class App:
+    # Course data
 
-    print(f"Course ID = {id_}, Course Name = {name_}, Today's progress' = {progress_}, Streak = {streak_}")
+    def course(self):
+        """Captures course details such as course name, goal, progress, and streak"""
+
+        course_list = new_login.get_course()
+        for course in course_list['courses']:
+            id_ = course['id']
+            name_ = course['name']
+            # some courses may not have goals attached to them.
+            try:
+                goal_, points_, streak_ = course['goal']['goal'], course['goal']['points'], course['goal']['streak']
+            except KeyError:
+                goal_, points_, streak_ = None, None, None
+            progress_ = str(points_) + "/" + str(goal_)
+
+            print(f"Course ID = {id_}, Course Name = {name_}, Today's progress' = {progress_}, Streak = {streak_}")
+
+    def graph(self):
+        """Capture and show activity over time"""
+        # Graph data
+        attendance_data = new_login.get_user_graph()['attendance_data']
+
+        data = []
+        for record in attendance_data:
+            data.append([record['day'], record['num_events']])
+
+        df = pd.DataFrame(data, columns=['Day', 'Activity'])
+        df.Day = pd.to_datetime(df.Day)
+
+        grouped = df.groupby(pd.Grouper(key='Day', freq='M')).sum()
+        # grouped.plot(x=grouped.index.month, kind='bar', stacked=True)
+        grouped.plot()
+        plt.show()
+
+
+if __name__ == "__main__":
+    item = App()
+    item.graph()
+
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Memrise-Python-Wrapper
-Trying to make a python wrapper for Memrise API. There's no official/documented API, so I'll try to add whatever I could find
+Trying to make a python wrapper for Memrise API. There's no official/documented API, so I'll try to add whatever I could find]
+
+Logs in to the account using user credentials.
+
